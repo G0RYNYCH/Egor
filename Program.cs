b9b6f731047dbdf7b6d84f9d7e3c27f9467b1a6b
GORYNTCHdotNET <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-02-26 22:44:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWars_Katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatStr(5, "Hi"));
+        }
+
+        /// <summary>
+        /// The Func returns true if str ends with ending.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="ending"></param>
+        /// <returns></returns>
+        public static bool Solution(string str, string ending) => str.EndsWith(ending);
+
+
+        /// <summary>
+        /// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+        /// </summary>
+        /// <param name="flower1"></param>
+        /// <param name="flower2"></param>
+        /// <returns></returns>
+        public static bool lovefunc(int flower1, int flower2) => flower1 % 2 != flower2 % 2;
+        //{
+        //    return (flower1 + flower2) % 2 == 1;            
+        //}
+
+        /// <summary>
+        /// Cocatenate strings n times.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string RepeatStr(int n, string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(s);
+            }            
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="distanceToPump"></param>
+        /// <param name="mpg"></param>
+        /// <param name="fuelLeft"></param>
+        /// <returns></returns>
+        public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft) => fuelLeft * mpg >= distanceToPump;        
+    }
+}
