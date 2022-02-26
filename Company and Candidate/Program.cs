aa82e0a933f926951a6a1d21cb65491658750d7d
GORYNTCHdotNET <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-02-26 11:31:48 +0300

    Add project files.

---
+++ Company and Candidate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Company_and_Candidate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Company company = new Company("Godel", 5);
+            Candidate candidate = new Candidate("NJ", 5);
+            company.Compare();
+        }
+    }
+
+     class Company
+    {
+        public string Name;
+        public int CompanySalary;
+
+        public Company(string name, int conmpanySalary)
+        {
+            Name = name;
+            CompanySalary = conmpanySalary;
+        }
+
+        public void Compare()
+        {
+            if (Candidate.CandidateSalary <= CompanySalary)
+            {
+                Console.WriteLine("+");
+            }
+            else
+            {
+                Console.WriteLine("-");
+            }
+        }
+    }
+
+    class Candidate
+    {
+        public static string Name;
+        public static int CandidateSalary;
+
+        public Candidate(string name, int candidatesalary)
+        {
+            Name = name;
+            CandidateSalary = candidatesalary;            
+        }
+    }
+
+}
